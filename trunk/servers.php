4a65a1d47bffcbc6bc4446f418deb4014825a041
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2008-03-28 22:48:01 +0000

    Code cleanup

--- trunk/servers.php
+++ trunk/servers.php
@@ -112,23 +112,6 @@
 				}
 			td.feature{
 /* 				font-size: 2em; */
-				<?php 
-// 					switch($OS){
-// // 						case 'WINDOWS':
-// // 							echo 'font-family: Webdings;';
-// // 							break;
-// // 						case 'LINUX':
-// // 							echo 'font-family: "DejaVu Sans";';
-// // 							break;
-// // 						case 'MACOSX':
-// // 							echo 'font-family: Webdings;';
-// // 							break;
-// 						default:
-// 							echo "height: 26px;";
-// // 							echo 'font-family: webdings;';
-// 							break;
-// 					}
-				?>
 			}
 			td.no{
 				color: #E90900;/*firebrick;*/
@@ -175,9 +158,6 @@
 				if(($result = $db->query($query)) === False) die('MySQL Error: '.$db->error());
 				
 				echo "<table>\n";
-// 				echo "\t<tr class=\"table_header\">\n";
-// 				foreach($fields as $field => $field_name) echo "\t\t<th class=\"$field\">$field_name</th>\n";
-// 				echo "\t</tr>\n";
 				$row = $result->fetch_assoc();
 				
 				
@@ -187,16 +167,6 @@
 						if($row_number%ROWS_BETWEEN_TITLES==0){
 							echo "\t<tr class=\"table_header\">\n";
 							foreach($fields as $field => $field_name){
-// 								switch($field){
-// 									case 'name':
-// 										echo "\t\t<th class=\"name\">".$field_name."</th>\n";
-// 										break;
-// 									case 'times_offline':
-// 										echo "\t\t<th class=\"times_offline\">".$field_name."</th>\n";
-// 										break;	
-// 									default:
-// 										echo "<th>".$field_name."</th>";
-// 								}
 								echo "<th class=\"$field_name".(($_GET['sort']==$field)?" sortedby":((!isset($_GET['sort']) && ('name'==$field))?" sortedby":""))."\"><a href=\"?sort=$field&amp;order=".((($_GET['order']==="desc")&&($_GET['sort']==$field))||(($field=='name')&&(!(($_GET['sort']==$field)&&($_GET['order']=='asc'))))?"asc":"desc")."\">".$field_name."</a></th>\n";
 							}
 							echo "\t</tr>\n";
@@ -221,24 +191,9 @@
 										echo "\t\t<td class=\"times_offline\">".htmlspecialchars($row[$field])."</td>\n";
 										break;	
 									default:
-// 										echo "\t\t<td>".($row[$field]?"<img src=\"images/button_ok.png\" width=\"22\" height=\"22\" title=\"Yes\" alt=\"Yes\" />":"<img src=\"images/button_cancel.png\" width=\"22\" height=\"22\" title=\"No\" alt=\"No\" />")."</td>\n";
 										switch($row[$field]){
 											case 255:  // Available
 												echo "\t\t<td class=\"feature yes ".$field."\">";
-	// 											switch($OS){
-	// 												case 'WINDOWS':
-	// 													echo 'a';
-	// 													break;
-	// 												case 'LINUX':
-	// 													echo '✔';
-	// 													break;
-	// 												case 'MACOSX':
-	// 													echo 'a';
-	// 													break;
-	// 												default:
-	// 													echo "<img src=\"images/button_ok.png\" width=\"22\" height=\"22\" title=\"Yes\" alt=\"Yes\" />";
-	// 													break;
-	// 											}
 												switch($field){
 													case 'has_muc':
 														echo "<img src=\"images/irc_protocol.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
@@ -276,9 +231,6 @@
 													case 'has_weather':
 														echo "<img src=\"images/weather.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
 														break;
-	// 												case 'has_msn':
-	// 													echo "<img src=\"images/msn_protocol.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
-	// 													break;
 													default:
 														echo "<img src=\"images/button_ok.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
 														break;
@@ -326,9 +278,6 @@
 													case 'has_weather':
 														echo "<img src=\"images/weather-grey.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
 														break;
-	// 												case 'has_msn':
-	// 													echo "<img src=\"images/msn_protocol-grey.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
-	// 													break;
 													default:
 														echo "<img src=\"images/button_ok-grey.png\" width=\"16\" height=\"16\" title=\"Yes\" alt=\"Yes\" />";
 														break;
@@ -339,20 +288,6 @@
 											case 0: //Uavailable
 											default:
 												echo "\t\t<td class=\"feature no ".$field."\">";
-	// 											switch($OS){
-	// 												case 'WINDOWS':
-	// 													echo 'r';
-	// 													break;
-	// 												case 'LINUX':
-	// 													echo '✘';
-	// 													break;
-	// 												case 'MACOSX':
-	// 													echo 'r';
-	// 													break;
-	// 												default:
-	// 													echo "<img src=\"images/button_cancel.png\" width=\"22\" height=\"22\" title=\"No\" alt=\"No\" />";
-	// 													break;
-	// 											}
 												echo "</td>\n";
 										}
 								}
