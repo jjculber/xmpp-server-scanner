4bf0f8fc4cb506686931524005603843cf1519a9
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2008-03-28 23:20:03 +0000

    Use local file until web file gets more complete

--- bot/update_servers.sh
+++ bot/update_servers.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /clientes/jabberes/servers/bot
-wget -qN http://www.jabber.org/basicservers.xml -O servers.xml
+#Use local file until web file gets more complete
+#wget -qN http://www.jabber.org/basicservers.xml -O servers.xml
 time php /clientes/jabberes/servers/bot/update_mysql_service_list_bot.php | tee /clientes/jabberes/servers/bot/log 
