940bff40c94e41764c37f949f3311424389fbe32
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2008-05-12 19:26:28 +0000

    Added availability of the server

--- branches/pybot-fullinfo/pybot/include/xml_file_generator.py
+++ branches/pybot-fullinfo/pybot/include/xml_file_generator.py
@@ -17,24 +17,32 @@ def generate(filename, servers):
 		server_element = doc.createElement("server")
 		server_element.setAttribute("jid", server[u'jid'])
 		
-		service_types = server['available_services'].keys()
-		service_types.extend(server['unavailable_services'].keys())
-		
-		for service_type in service_types:
-			if service_type in server['available_services']:
-				for component in server['available_services'][service_type]:
-					component_element = doc.createElement("component")
-					component_element.setAttribute("jid", component)
-					component_element.setAttribute("type", service_type)
-					component_element.setAttribute("available", "yes")
-					server_element.appendChild(component_element)
-			if service_type in server['unavailable_services']:
-				for component in server['unavailable_services'][service_type]:
-					component_element = doc.createElement("component")
-					component_element.setAttribute("jid", component)
-					component_element.setAttribute("type", service_type)
-					component_element.setAttribute("available", "no")
-					server_element.appendChild(component_element)
+		if (  len(server['available_services']) == 0 and
+		      len(server['unavailable_services']) == 0 and
+		      len(server[u'info'][0]) == 0 and
+		      len(server[u'info'][1]) == 0  ):
+			server_element.setAttribute("available", "no")
+		else:
+			server_element.setAttribute("available", "yes")
+			
+			service_types = server['available_services'].keys()
+			service_types.extend(server['unavailable_services'].keys())
+			
+			for service_type in service_types:
+				if service_type in server['available_services']:
+					for component in server['available_services'][service_type]:
+						component_element = doc.createElement("component")
+						component_element.setAttribute("jid", component)
+						component_element.setAttribute("type", service_type)
+						component_element.setAttribute("available", "yes")
+						server_element.appendChild(component_element)
+				if service_type in server['unavailable_services']:
+					for component in server['unavailable_services'][service_type]:
+						component_element = doc.createElement("component")
+						component_element.setAttribute("jid", component)
+						component_element.setAttribute("type", service_type)
+						component_element.setAttribute("available", "no")
+						server_element.appendChild(component_element)
 		
 		servers_element.appendChild(server_element)
 		
