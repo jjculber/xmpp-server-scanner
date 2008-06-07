4c93a9734908c0c60b7770371c7c0ea6d0c8a1c6
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2008-06-07 15:10:27 +0000

    Added configuration variables for enable DB updating, HTML and XML file generation. Detect when MySQLdb is not available and don't try to update the database. Aded a debug option to load the disco info from the dump file instead from the real discovery.

--- trunk/pybot/pybot.py
+++ trunk/pybot/pybot.py
@@ -27,7 +27,13 @@ import urllib
 from include.xmpp import simplexml
 
 from include import xmpp_discoverer
-from include import database_updater
+
+try:
+	from include import database_update
+	CAN_UPDATE_DATABASE = True
+except ImportError:
+	CAN_UPDATE_DATABASE = False
+	
 from include import html_file_generator, xml_file_generator
 
 
@@ -40,33 +46,42 @@ from include import html_file_generator, xml_file_generator
 #jabberserver="jabberes.org"
 
 # Jabber account
-JABBERUSER     = "xxxxxxx"
-JABBERPASSWORD = "xxxxxxx"
-JABBERRESOURCE = "pybot"
-JABBERSERVER   = "jabber.example.com"
+JABBERUSER          = "xxxxxxx"
+JABBERPASSWORD      = "xxxxxxx"
+JABBERRESOURCE      = "pybot"
+JABBERSERVER        = "jabber.example.com"
 
 # Database
-DBUSER         = "user"
-DBPASSWORD     = "sql_password"
-DBHOST         = "localhost"
-DBDATABASE     = "server_list"
+DBUSER              = "user"
+DBPASSWORD          = "sql_password"
+DBHOST              = "localhost"
+DBDATABASE          = "server_list"
+
+UPDATE_DATABASE     = True
+GENERATE_HTML_FILES = True
+GENERATE_XML_FILES  = True
+
 
 # OUTPUT CONFIGURATION
-OUTPUT_DIRECTORY = '..'
+OUTPUT_DIRECTORY    = '..'
 
 # Server list
-USEURL         = False
-SERVERS_URL    = "http://www.jabber.org/basicservers.xml"
-#SERVERS_FILE   = "servers-fixed.xml"
-SERVERS_FILE   = 'servers-fixed.xml'
+USEURL              = False
+SERVERS_URL         = "http://www.jabber.org/basicservers.xml"
+#SERVERS_FILE       = "servers-fixed.xml"
+SERVERS_FILE        = 'servers-fixed.xml'
 
 # Logs
-#LOGFILE        = 'out.log'
-LOGFILE        = 'out.log'
-#LOGFILE        = None
+#LOGFILE            = 'out.log'
+LOGFILE             = 'out.log'
+#LOGFILE            = None
+
+# Debug
+# If false, load the discovery results from servers.dump file,
+# instead waiting while doing the real discovery
+DO_DISCOVERY        = False
 
 # Configuration end
-
 
 
 SCRIPT_DIR = abspath(dirname(sys.argv[0]))
@@ -129,65 +144,73 @@ for item in items:
 #server_list=['jabberes.org', 'jab.undernet.cz', '12jabber.com', 'allchitchat.com', 'jabber.dk', 'amessage.be', 'jabber-hispano.org', 'example.net']
 #server_list=['jabberes.org']
 
-servers = xmpp_discoverer.discover_servers( JABBERUSER, JABBERPASSWORD,
-                                            JABBERRESOURCE, JABBERSERVER,
-                                            server_list
-                                          )
-
-
-# Manage offline servers and stability information
-
-offline = lambda server: len(server[u'info'][0]) == 0 and len(server[u'info'][1]) == 0
-
-try:
-	f = open(SERVERS_DUMP_FILE, 'rb')
-	old_servers = pickle.load(f)
-	f.close()
+if DO_DISCOVERY:
+	servers = xmpp_discoverer.discover_servers( JABBERUSER, JABBERPASSWORD,
+	                                            JABBERRESOURCE, JABBERSERVER,
+	                                            server_list
+	                                          )
 	
-except IOError:
-	logging.warning("Error loading servers data in file servers.dump")
-	for server in servers.itervalues():
-		if offline(server):
-			server['offline_since'] = time.gmtime()
-			server['times_queried_online'] = 0
-			server['times_queried'] = 1
-		else:
-			server['offline_since'] = None
-			server['times_queried_online'] = 1
-			server['times_queried'] = 1
 	
-else:
-	for jid, server in servers.iteritems():
-		if offline(server):
-			try:
-				servers[jid] = old_servers[jid]
-				servers[jid]['times_queried'] += 1
-				if servers[jid]['offline_since'] is None:
-					servers[jid]['offline_since'] = time.gmtime()
-				logging.warning("%s server seems to be offline, using old data", jid)
-			except KeyError: # It's a new server
-				logging.debug("Initializing stability data for %s", jid)
+	# Manage offline servers and stability information
+	
+	offline = lambda server: len(server[u'info'][0]) == 0 and len(server[u'info'][1]) == 0
+	
+	try:
+		f = open(SERVERS_DUMP_FILE, 'rb')
+		old_servers = pickle.load(f)
+		f.close()
+		
+	except IOError:
+		logging.warning("Error loading servers data in file servers.dump. Is the script executed for first time?", exc_info=sys.exc_info())
+		for server in servers.itervalues():
+			if offline(server):
 				server['offline_since'] = time.gmtime()
 				server['times_queried_online'] = 0
 				server['times_queried'] = 1
-		else:
-			server['offline_since'] = None
-			try:
-				server['times_queried_online'] = old_servers[jid]['times_queried_online'] + 1
-				server['times_queried'] =  old_servers[jid]['times_queried'] + 1
-			except KeyError: # It's a new server
-				logging.debug("Initializing stability data for %s", jid)
+			else:
+				server['offline_since'] = None
 				server['times_queried_online'] = 1
 				server['times_queried'] = 1
-	
-finally:
+		
+	else:
+		for jid, server in servers.iteritems():
+			if offline(server):
+				try:
+					servers[jid] = old_servers[jid]
+					servers[jid]['times_queried'] += 1
+					if servers[jid]['offline_since'] is None:
+						servers[jid]['offline_since'] = time.gmtime()
+					logging.warning("%s server seems to be offline, using old data", jid)
+				except KeyError: # It's a new server
+					logging.debug("Initializing stability data for %s", jid)
+					server['offline_since'] = time.gmtime()
+					server['times_queried_online'] = 0
+					server['times_queried'] = 1
+			else:
+				server['offline_since'] = None
+				try:
+					server['times_queried_online'] = old_servers[jid]['times_queried_online'] + 1
+					server['times_queried'] =  old_servers[jid]['times_queried'] + 1
+				except KeyError: # It's a new server
+					logging.debug("Initializing stability data for %s", jid)
+					server['times_queried_online'] = 1
+					server['times_queried'] = 1
+		
+	finally:
+		try:
+			f = open(SERVERS_DUMP_FILE, 'wb')
+			pickle.dump(servers, f, -1)
+			f.close()
+		except IOError:
+			logging.error("Error saving servers data in servers.dump")
+else:
 	try:
-		f = open(SERVERS_DUMP_FILE, 'wb')
-		pickle.dump(servers, f, -1)
+		f = open(SERVERS_DUMP_FILE, 'rb')
+		servers = pickle.load(f)
 		f.close()
 	except IOError:
-		logging.error("Error saving servers data in servers.dump")
-
+		logging.critical("Error loading servers data in file servers.dump", exc_info=sys.exc_info())
+		raise
 
 #for jid, server in sorted(servers.iteritems()):
 	
@@ -211,8 +234,11 @@ finally:
 
 # Now dump the information to the dataabase
 
-database_updater.update_database( DBUSER, DBPASSWORD, DBHOST,
-                                  DBDATABASE, servers )
+if UPDATE_DATABASE and not CAN_UPDATE_DATABASE:
+	logging.error("Can't update the database. Is MySQLdb module available?")
+elif UPDATE_DATABASE and CAN_UPDATE_DATABASE:
+	database_updater.update_database( DBUSER, DBPASSWORD, DBHOST,
+	                                  DBDATABASE, servers )
 
 
 # And build the HTML pages and the XML
@@ -223,9 +249,11 @@ columns = [ 'muc', 'irc', 'aim', 'gadu-gadu', 'icq', 'msn',
 
 #known_types.sort()
 #html_file_generator.generate('../servers-pybot.html', servers, known_types)
-html_file_generator.generate_all( directory=OUTPUT_DIRECTORY,
-                                  filename_prefix=HTML_FILES_PREFIX,
-                                  servers=servers, types=columns,
-                                  compress=True )
+if GENERATE_HTML_FILES:
+	html_file_generator.generate_all( directory=OUTPUT_DIRECTORY,
+	                                  filename_prefix=HTML_FILES_PREFIX,
+	                                  servers=servers, types=columns,
+	                                  compress=True )
 
-xml_file_generator.generate(XML_FILE, servers)
+if GENERATE_XML_FILES:
+	xml_file_generator.generate(XML_FILE, servers)
