21e963dda2575b3a4d424dbf0675e16ebd8e2e62
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2008-07-13 16:42:45 +0000

    Fix component handling on older servers, (a change forgotten in r192)

--- trunk/pybot/include/xmpp_discoverer.py
+++ trunk/pybot/include/xmpp_discoverer.py
@@ -371,7 +371,7 @@ def _discover_item(dispatchers, component, server):
 	
 	elif u'jabber:iq:agents' in component[u'info'][1]:
 		#Fake identities. But we aren't really sure that it's a server?
-		component[u'info'] = ( ({u'category': u'server', u'type': u'im'}),
+		component[u'info'] = ( [{u'category': u'server', u'type': u'im'}],
 		                     component[u'info'][1] )
 		_handle_component_available(component, server)
 		needs_to_query_items = True
