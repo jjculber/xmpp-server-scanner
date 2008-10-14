d74a48b81598cd614dc99fa49803a379ce3d3058
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2008-10-14 16:33:59 +0000

    Add support for uptime filtering (Based on pybot-coccinella branch)

--- pybot/include/xml_file_generator.py
+++ pybot/include/xml_file_generator.py
@@ -5,8 +5,14 @@ import logging
 import shutil
 from xml.dom.minidom import getDOMImplementation
 
-def generate(filename, servers):
+def generate(filename, servers, minimun_uptime=0):
 	"""Generate a XML file with the information stored in servers"""
+	
+	if minimun_uptime>0:
+		# Filter by uptime
+		_servers = {}
+		_servers.update([(k,v) for k,v in servers.iteritems() if float(v['times_queried_online'])/v['times_queried'] > minimun_uptime])
+		servers = _servers
 	
 	tmpfilename = filename + '.tmp'
 	logging.info('Generating XML file "%s"', tmpfilename)
