8f00df83089d51e41f2fd4c06bdf9336dc4303fe
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2009-01-26 14:59:10 +0000

    Compatible again with python2.4 and a fix

--- branches/pybot-python24/pybot/include/html_file_generator.py
+++ branches/pybot-python24/pybot/include/html_file_generator.py
@@ -454,7 +454,7 @@ def generate( filename, servers, types, sort_by=None, sort_links=None,
 	)
 	
 	# Apply a different style to sorted columns
-	sort_class = sort_class if not isinstance(sort_class, tuple) else "%s_%s" % sort_class
+	sort_class = isinstance(sort_by, tuple) and "%s_%s" % sort_by or sort_by
 	f.write(
 """
 			tr.table_header th.%s{
