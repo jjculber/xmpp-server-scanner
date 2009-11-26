18ebcd48c4a79378d7561b5652b1f17786cd3fa6
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2009-11-26 19:43:57 +0000

    Replace the deprecated functions in xmpppy

--- trunk/pybot/include/xmpp/transports.py
+++ trunk/pybot/include/xmpp/transports.py
@@ -27,7 +27,7 @@ Transports are stackable so you - f.e. TLS use HTPPROXYsocket or TCPsocket as mo
 Also exception 'error' is defined to allow capture of this module specific exceptions.
 """
 
-import socket,select,base64,dispatcher,sys
+import socket,ssl,select,base64,dispatcher,sys
 from simplexml import ustr
 from client import PlugIn
 from protocol import *
@@ -144,7 +144,7 @@ class TCPsocket(PlugIn):
         """ Reads all pending incoming data.
             In case of disconnection calls owner's disconnected() method and then raises IOError exception."""
         try: received = self._recv(BUFLEN)
-        except socket.sslerror,e:
+        except ssl.SSLError,e:
             self._seen_data=0
             if e[0]==2: return ''
             self.DEBUG('Socket error while receiving data','error')
@@ -302,9 +302,9 @@ class TLS(PlugIn):
         """ Immidiatedly switch socket to TLS mode. Used internally."""
         """ Here we should switch pending_data to hint mode."""
         tcpsock=self._owner.Connection
-        tcpsock._sslObj    = socket.ssl(tcpsock._sock, None, None)
-        tcpsock._sslIssuer = tcpsock._sslObj.issuer()
-        tcpsock._sslServer = tcpsock._sslObj.server()
+        tcpsock._sslObj    = ssl.wrap_socket(tcpsock._sock, None, None)
+        #tcpsock._sslIssuer = tcpsock._sslObj.issuer()
+        #tcpsock._sslServer = tcpsock._sslObj.server()
         tcpsock._recv = tcpsock._sslObj.read
         tcpsock._send = tcpsock._sslObj.write
 
