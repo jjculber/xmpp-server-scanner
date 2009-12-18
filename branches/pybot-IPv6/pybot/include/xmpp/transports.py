204576337b8ad351779bb56493fa2c27bcc6b451
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2009-12-18 01:59:23 +0000

    Added quick and dirty IPv6 support from SourceForge Feature Request ID: 2851611 ( http://sourceforge.net/tracker/index.php?func=detail&aid=2851611&group_id=97081&atid=616918 ) I have not tested it

--- branches/pybot-IPv6/pybot/include/xmpp/transports.py
+++ branches/pybot-IPv6/pybot/include/xmpp/transports.py
@@ -122,8 +122,14 @@ class TCPsocket(PlugIn):
         """ Try to connect. Returns non-empty string on success. """
         try:
             if not server: server=self._server
-            self._sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self._sock.connect((server[0], int(server[1])))
+            try:
+              self._sock.connect((server[0], int(server[1])))
+              self.DEBUG("IPv6 is in use",'start')
+            except socket.error:
+              self._sock.close();
+              self._sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+              self._sock.connect((server[0], int(server[1])))
+
             self._send=self._sock.sendall
             self._recv=self._sock.recv
             self.DEBUG("Successfully connected to remote host %s"%`server`,'start')
