5d7fc7831119e5a244315642f2e40c14781d4823
lambda512 <lambda512@87a4c8e6-76cd-11de-aab9-69570e6b7754>
2009-12-18 13:34:44 +0000

    Reintegrate the pybot-IPv6 branch in trunk. Connections to IPv6 servers and IPv6 server readiness should be supported now

--- trunk/pybot/include/ipv6_aux.py
+++ trunk/pybot/include/ipv6_aux.py
@@ -13,13 +13,17 @@
 # If it fails, the server name (jabber.org) will be used
 # Then, make an AAAA query to get the IPv6 ip, if there is one, mark as IPv6 ready
 # Ideally it should be checked with a telnet-like connection to see if the server implementation is IPv6 ready
-# but since it would require to run in a IPv6-ready machine, this won't be implemented
-# TODO: Check if we are in a IPv6-ready machine and create a socket to test the connection
+# but since it would require to run in a IPv6-ready machine, this step will be skipped if IPv6 is not available
 
 # DNS SRV resolver code taken from xmpppy
 
 import logging
+import socket
 import sys
+
+HAVE_IPv6 = socket.has_ipv6
+if not HAVE_IPv6:
+	logging.warning("There is no support for IPv6 in this machine. IPv6 connection test will be skipped.")
 
 # determine which DNS resolution library is available
 HAVE_DNSPYTHON = False
@@ -42,10 +46,11 @@ except ImportError:
 		raise
 
 
-def get_server_cname(server, use_srv=True):
+def get_server_host_port(host, use_srv=True):
+	"""Get the host and port where the server is running. By default, use DNS SRV for that."""
+	port = 5222
 	# SRV resolver
 	if use_srv and (HAVE_DNSPYTHON or HAVE_PYDNS):
-		host = server
 		possible_queries = ['_xmpp-client._tcp.' + host]
 
 		for query in possible_queries:
@@ -70,41 +75,71 @@ def get_server_cname(server, use_srv=True):
 			except:
 				#TODO: use self.DEBUG()
 				logging.error('An error occurred while looking up %s' % query, exc_info=sys.exc_info())
-		server = host
 	# end of SRV resolver
-	return server
+	return host, port
 
 
-def resolve_ipv6(cname):
+def resolve_ipv6(host):
+	"""Get the IPv6 address of the host"""
 	try:
 		if HAVE_DNSPYTHON:
 			try:
-				answers = [x for x in dns.resolver.query(cname, 'AAAA')]
+				answers = [x for x in dns.resolver.query(host, 'AAAA')]
 			except dns.resolver.NoAnswer:
 				return None
 			if answers:
-				host = str(answers[0].address)
+				ipv6 = str(answers[0].address)
 		elif HAVE_PYDNS:
 			# ensure we haven't cached an old configuration
 			DNS.ParseResolvConf()
-			response = DNS.Request().req(cname, qtype='AAAA')
+			response = DNS.Request().req(host, qtype='AAAA')
 			answers = response.answers
 			raise NotImplementedError('PyDNS has no support for IPv6 yet.')
 			if len(answers) > 0:
 				# ignore the priority and weight for now
-				_, _, port, host = answers[0]['data']
+				_, _, port, ipv6 = answers[0]['data']
 				del _
 				port = int(port)
 	except:
 		#TODO: use self.DEBUG()
-		logging.error('An error occurred while looking up %s' % cname, exc_info=sys.exc_info())
+		logging.error('An error occurred while looking up %s' % host, exc_info=sys.exc_info())
 		return None
 	else:
-		return host
-
-def get_server_ipv6(server):
-	return resolve_ipv6(get_server_cname(server))
-
+		return ipv6
 
 def is_ipv6_ready(server):
-	return resolve_ipv6(get_server_cname(server)) is not None
+	"""Check if the server is IPv6-ready.
+	In order to test it, we look for his IPv6 address
+		If it doesn't have one, it is not IPv6-ready.
+		If it have one and our machine is IPv6-ready, then try to stablish a connection.
+		If it have one and our machine is not IPv6-ready, then trust the DNS record and believe that is IPv6-ready."""
+	
+	global HAVE_IPv6
+	host, port = get_server_host_port(server)
+	ipv6 = resolve_ipv6(host)
+	
+	if ipv6 is None:
+		return False # There is no DNS record
+	
+	if not HAVE_IPv6:
+		return True # We can't test the connection, so we trust the DNS record
+	
+	logging.debug("Trying to connect using IPv6 to %s: %s, %s,%d." % (server, host, ipv6, port))
+	try:
+		s = socket.create_connection((ipv6, port))
+		s.close()
+	except socket.error as err:
+		if err.errno == 97: # errno.EAFNOSUPPORT ([Errno 97] Address family not supported by protocol)
+			HAVE_IPv6 = False
+			logging.warning("There is no support for IPv6 in this machine. IPv6 connection test will be skipped.")
+			return True # We can't test the connection, so we trust the DNS record
+		elif err.errno == 111: # errno.ECONNREFUSED ([Errno 111] Connection refused)
+			# The server implementation doesn't have IPv6 support
+			return False
+		else:
+			# Unknown error
+			logging.error("The socket has failed while trying to connect to %s: %s, %s,%d." % (server, host, ipv6, port), exc_info=sys.exc_info())
+			return False
+	else:
+		return True
+	
